06a0caae7cff98b7bfeb1fc30c98143a9fd889e4
Abhishek Singh <3asingh87@gmail.com>
2021-05-12 01:14:32 +0530

    Add log to DefaultRestApi
    Change System.out.println to Spring Log

--- src/main/java/com/example/demo/TestApp.java
+++ src/main/java/com/example/demo/TestApp.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping(value = "/api")
 public class TestApp {
+	private Logger logger = LoggerFactory.getLogger(TestApp.class);
+	
 	@RequestMapping(value = "/welcome")
 	public String welcome() {
+		logger.info("Hello This is Spring Boot Rest Api");
 		return "Welcome in Java world";
 	}
 
